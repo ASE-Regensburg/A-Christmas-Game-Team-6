5edd9dd3fa68ae979a3b4a7ca5ad005ca61400d5
Alexander Bazo <alexanderbazo@googlemail.com>
2019-12-20 07:50:30 +0100

    Add scene model

---
+++ src/scenes/Scene.java
@@ -0,0 +1,42 @@
+package scenes;
+
+import config.GameConfig;
+import de.ur.mi.oop.events.KeyPressedEvent;
+import de.ur.mi.oop.graphics.Image;
+
+public class Scene implements GameConfig {
+
+    private String tag;
+    private SceneListener listener;
+    private Image backgroundImage;
+
+    public Scene(String tag, String backgroundImagePath, SceneListener listener) {
+        this.tag = tag;
+        this.listener = listener;
+        this.backgroundImage = new Image(0,0,backgroundImagePath);
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public SceneListener getListener() {
+        return listener;
+    }
+
+    public void initialize() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void draw() {
+        this.backgroundImage.draw();
+    }
+
+    public void onKeyPressed(KeyPressedEvent event) {
+
+    }
+}
