833d173fc918db77e7e240a86e699913e6545c6d
Alexander Bazo <alexanderbazo@googlemail.com>
2019-12-20 07:54:25 +0100

    Implement and set intro scene

--- src/config/GameConfig.java
+++ src/config/GameConfig.java
@@ -5,5 +5,6 @@ public interface GameConfig {
     public static final int SCREEN_WIDTH = 320;
     public static final int SCREEN_HEIGHT = 288;
     public static final int TARGET_FPS = 60;
+    public static final String INTRO_SCENE_TAG = "Intro";
 
 }
